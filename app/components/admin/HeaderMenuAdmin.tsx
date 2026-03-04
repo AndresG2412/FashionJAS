22cfe0d212d463efeb21f2581fb2223e98d6b999
Andres Gaviria <cgaviria930@gmail.com>
2026-03-04 15:09:53 -0500

    header admin diseño nuevo

--- app/components/admin/HeaderMenuAdmin.tsx
+++ app/components/admin/HeaderMenuAdmin.tsx
@@ -8,17 +8,17 @@ export default function HeaderMenuAdmin() {
     const pathname = usePathname();
     
     return (
-        <div className='hidden md:flex w-1/3 items-center justify-center gap-7 capitalize font-normal text-lightColor'>
+        <div className='hidden md:flex w-1/3 items-center justify-center gap-7 capitalize font-normal text-danashop-textPrimary'>
             {headerDataAdmin?.map((item, index) => (
                 <Link 
                     key={index} 
                     href={item.href} 
-                    className={`hover:text-shop_light_green hoverEffect font-semibold tracking-wide relative group ${pathname === item?.href ? 'text-shop_light_green' : ''}`}
+                    className={`hover:text-danashop-brandSoft hoverEffect font-semibold tracking-wide relative group ${pathname === item?.href ? 'text-danashop-brandSoft' : ''}`}
                 >
                     {item.title}
                     
                     <span 
-                        className={`absolute -bottom-0.5 left-1/2 h-0.5 bg-shop_light_green hoverEffect -translate-x-1/2 
+                        className={`absolute -bottom-0.5 left-1/2 h-0.5 bg-danashop-brandSoft hoverEffect -translate-x-1/2 
                         group-hover:w-full ${pathname === item?.href ? 'w-full' : 'w-0'}`}
                     />
                 </Link>
